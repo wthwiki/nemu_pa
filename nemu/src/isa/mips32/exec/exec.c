ecde076da524981695e90c9cdf040ca15619b4d8
tracer-ics2020 <tracer@njuics.org>
2022-07-31 16:33:48 +0800

    >  compile 2020070850    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 02aca08053754b59983da4f618bc1d9c            Boot ID: 6196edf3b3d44092955b8d601dc11c8c     Virtualization: vmware   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-15-amd64       Architecture: x86-64  16:33:48 up 18:48,  3 users,  load average: 0.31, 0.31, 0.27

--- nemu/src/isa/mips32/exec/exec.c
+++ nemu/src/isa/mips32/exec/exec.c
@@ -15,8 +15,9 @@ static inline def_EHelper(special) {
 
 static inline void fetch_decode_exec(DecodeExecState *s) {
   // instruction fetch IF
+  
   s->isa.instr.val = instr_fetch(&s->seq_pc, 4);
-
+  printf("pc. x=%u\n",s->isa.instr.val);
   switch (s->isa.instr.r.opcode ) {
     EX   (000, special)
     IDEX (017, IU, lui)
