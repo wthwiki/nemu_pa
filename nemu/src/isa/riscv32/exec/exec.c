c31d0e5d8b53c3c4cf8e245bda1ed026beb0fead
tracer-ics2020 <tracer@njuics.org>
2022-08-01 16:08:15 +0800

    >  compile 2020070850    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 02aca08053754b59983da4f618bc1d9c            Boot ID: 6196edf3b3d44092955b8d601dc11c8c     Virtualization: vmware   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-15-amd64       Architecture: x86-64  16:08:15 up 1 day,  6:08,  1 user,  load average: 0.05, 0.13, 0.20

--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -48,7 +48,7 @@ static inline void fetch_decode_exec(DecodeExecState *s) {
     IDEX (0b00000, I, load)
     IDEX (0b00100, I, lui) // wth
     IDEX (0b01000, S, store)
-    IDEX (0b00101, I, store)
+    IDEX (0b00101, U, store)
     IDEX (0b01101, U, lui)
     EX   (0b11010, nemu_trap)
     default: exec_inv(s);
