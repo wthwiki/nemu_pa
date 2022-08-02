e4c5e07792123d06fe53918adad4b8194293ba43
tracer-ics2020 <tracer@njuics.org>
2022-08-02 15:45:19 +0800

    >  compile 2020070850    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 02aca08053754b59983da4f618bc1d9c            Boot ID: 6196edf3b3d44092955b8d601dc11c8c     Virtualization: vmware   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-15-amd64       Architecture: x86-64  15:45:19 up 1 day, 18:09,  1 user,  load average: 0.22, 0.32, 0.31

--- nemu/src/isa/riscv32/exec/ldst.h
+++ nemu/src/isa/riscv32/exec/ldst.h
@@ -11,7 +11,7 @@ static inline def_EHelper(ld) {
 }
 
 static inline def_EHelper(st) {
-  printf("id_src2->imm=%x\n",id_src2->imm);
+  printf("id_src2->imm=%x,dsrc1=%x\n",id_src2->imm,*dsrc1);
   rtl_sm(s, dsrc1, id_src2->imm, ddest, s->width);
 
   print_Dop(id_src1->str, OP_STR_SIZE, "%d(%s)", id_src2->imm, reg_name(id_src1->reg));
