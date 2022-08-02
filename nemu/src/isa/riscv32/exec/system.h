cbaf480d5d3ebe8994b9e38cf83054f6c71041d2
tracer-ics2020 <tracer@njuics.org>
2022-08-02 16:15:44 +0800

    >  compile 2020070850    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 02aca08053754b59983da4f618bc1d9c            Boot ID: 6196edf3b3d44092955b8d601dc11c8c     Virtualization: vmware   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-15-amd64       Architecture: x86-64  16:15:44 up 1 day, 18:39,  1 user,  load average: 0.32, 0.26, 0.27

--- nemu/src/isa/riscv32/exec/system.h
+++ nemu/src/isa/riscv32/exec/system.h
@@ -1,8 +1,8 @@
 //I wth  t =pc+4; pc=(x[rs1]+sext(offset))&~1; x[rd]=t
 static inline def_EHelper(jalr) {
-  s->seq_pc=(*dsrc1+id_src2->imm)&(~1);
   rtl_li(s, ddest, s->seq_pc+4);
-  
+  s->seq_pc=(*dsrc1+id_src2->imm)&(~1);
+
   print_asm_template2(jalr);
 }
 // I wth 
