392e7b7244d14727f1f1d90bb13803f67c38e038
tracer-ics2020 <tracer@njuics.org>
2022-08-02 15:54:11 +0800

    >  compile 2020070850    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 02aca08053754b59983da4f618bc1d9c            Boot ID: 6196edf3b3d44092955b8d601dc11c8c     Virtualization: vmware   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-15-amd64       Architecture: x86-64  15:54:11 up 1 day, 18:18,  1 user,  load average: 0.15, 0.31, 0.31

--- nemu/src/isa/riscv32/exec/muldiv.h
+++ nemu/src/isa/riscv32/exec/muldiv.h
@@ -1,6 +1,6 @@
 // wth I
 static inline def_EHelper(addi) {
-  printf("dsrc1=%x\n",*ddest);
+  printf("addi_______dsrc1=%x\n",*ddest);
   rtl_addi(s, ddest, dsrc1,id_src2->imm);
   print_asm_template2(addi);
 }
