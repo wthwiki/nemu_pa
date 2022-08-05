6e0e979a4fa39955cd55485796560834c33f11e6
2020070850-wth <2086385336@qq.com>
2022-08-05 09:35:32 +0800

    first commit

--- abstract-machine/am/src/nemu/trm.c
+++ abstract-machine/am/src/nemu/trm.c
@@ -4,16 +4,18 @@
 extern char _heap_start;
 int main(const char *args);
 
+// heap 结构用于指示堆区的起始和末尾
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
 
+// 用于输出一个字符
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
 }
-
+// 用于结束程序的运行
 void halt(int code) {
   nemu_trap(code);
 
@@ -21,6 +23,7 @@ void halt(int code) {
   while (1);
 }
 
+// 用于进行TRM相关的初始化工作
 void _trm_init() {
   int ret = main(mainargs);
   halt(ret);
